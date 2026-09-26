b0c84083230ae8ae206d926b675a8c46f61831a9
hoang tran <ngochoangnd95@gmail.com>
2026-09-26 10:23:14 +0700

    Initialize "akita-api" Elysia app running on Bun runtime

---
+++ apps/akita-api/src/index.ts
@@ -0,0 +1,8 @@
+import { Elysia } from 'elysia';
+import { env } from './env';
+
+const app = new Elysia().get('/', () => 'Hello Elysia').listen(env.PORT);
+
+console.log(
+	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
+);
