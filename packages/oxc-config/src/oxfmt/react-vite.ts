abc818b3593662247f91f0478f090cc951fdc863
hoang tran <ngochoangnd95@gmail.com>
2026-09-26 10:03:11 +0700

    Create shared Biome configuration package

--- packages/oxc-config/src/oxfmt/react-vite.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig, type OxfmtConfig } from 'oxfmt';
-
-import baseConfig from './base';
-
-export default defineConfig({
-	...baseConfig,
-	jsxSingleQuote: true,
-	sortTailwindcss: true,
-}) as OxfmtConfig;
